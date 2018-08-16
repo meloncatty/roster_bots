a296e43a68ff3170a9727c6a3c989dee58ad48cc
Krista Handel <k.handel66@gmail.com>
2018-08-16 11:04:52 -0600

    Add form handlers

--- src/components/RosterForm/index.js
+++ src/components/RosterForm/index.js
@@ -9,12 +9,27 @@ class RosterForm extends Component {
     }
   }
 
+  handleOnChange = e => {
+    const { value, name } = e.target
+    this.setState({ [name] : value })
+  }
+
+  handleOnSubmit = e => {
+    e.preventDefault()
+  }
+
   render() {
     return (
       <form>
         <label for='team-name'>Team Name</label>
-        <input id='team-name' type='text' name='teamName' placeholder='Champs' required/>
-        <button type='submit'>Generate Team</button>
+        <input
+          id='team-name'
+          type='text'
+          name='teamName'
+          placeholder='Champs'
+          onChange={this.handleOnChange}
+          required/>
+        <button type='submit' disabled={!this.state.teamName}>Generate Team</button>
       </form>
     )
   }
