04818ed11e1aabd9836504fcce0e8db23a3bf70f
Krista Handel <k.handel66@gmail.com>
2018-08-16 10:44:06 -0600

    Create form container skeleton Create components index for easy import/export

---
+++ src/components/RosterForm/index.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+
+class RosterForm extends Component {
+  constructor() {
+    super()
+
+    this.state = {
+      teamName: ''
+    }
+  }
+
+  render() {
+    return (
+      <div />
+    )
+  }
+}
+
+export default RosterForm
