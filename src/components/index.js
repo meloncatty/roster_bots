1ae3e6b1a354ff385147e714d7a7e3f1000ac033
Krista Handel <k.handel66@gmail.com>
2018-08-17 17:53:22 -0600

    Add SubstituteCards to index

--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,9 @@
 import RosterForm from './RosterForm'
 import StarterCards from './StarterCards'
+import SubstituteCards from './SubstituteCards'
 
 export {
   RosterForm,
-  StarterCards
+  StarterCards,
+  SubstituteCards
 }
