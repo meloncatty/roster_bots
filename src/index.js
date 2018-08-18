fe204b9d36703f94c91943f056f4a64fea41e672
Krista Handel <k.handel66@gmail.com>
2018-08-17 20:16:21 -0600

    Connect App to store

--- src/index.js
+++ src/index.js
@@ -1,13 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import {BrowserRouter as Router} from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import rootReducer from './reducers'
 import './index.css'
 import App from './App'
 
+const store = createStore(rootReducer)
+
 const app = (
-  <Router>
-    <App />
-  </Router>
+  <Provider store={store}>
+    <Router>
+      <App />
+    </Router>
+  </Provider>
 );
 
 ReactDOM.render(app, document.getElementById('root'));
