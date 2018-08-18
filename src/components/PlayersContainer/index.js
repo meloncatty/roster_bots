f4b3528be2856e20c6579e9184a0a6ec868c7358
Krista Handel <k.handel66@gmail.com>
2018-08-17 20:18:01 -0600

    Connect PlayersContainer to store

--- src/components/PlayersContainer/index.js
+++ src/components/PlayersContainer/index.js
@@ -1,20 +1,32 @@
-import React from 'react'
+import React, { Component } from 'react'
+import { connect } from 'react-redux';
 import { StarterCards, SubstituteCards } from '../'
 import './styles.css'
 
-const PlayersContainer = () => {
-  return (
-    <main className='cards-container'>
-      <h2>Starters</h2>
-      <section className='starters-container'>
-        <StarterCards />
-      </section>
-      <h2>Substitutes</h2>
-      <section className='substitutes-container'>
-        <SubstituteCards />
-      </section>
-    </main>
-  )
+class PlayersContainer extends Component {
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
+    console.log(this.props.teamName)
+    return (
+      <main className='cards-container'>
+        <h2>Starters</h2>
+        <section className='starters-container'>
+          <StarterCards />
+        </section>
+        <h2>Substitutes</h2>
+        <section className='substitutes-container'>
+          <SubstituteCards />
+        </section>
+      </main>
+    )
+  }
 }
 
-export default PlayersContainer
+export const mapStateToProps = state => ({
+  teamName: state.teamName
+})
+
+export default connect(mapStateToProps)(PlayersContainer)
