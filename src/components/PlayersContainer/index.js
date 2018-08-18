f8a0d8fe0eab81d8ba1aa657e96292e4424aee02
Krista Handel <k.handel66@gmail.com>
2018-08-17 19:06:52 -0600

    Add styles

--- src/components/PlayersContainer/index.js
+++ src/components/PlayersContainer/index.js
@@ -1,14 +1,17 @@
 import React from 'react'
-import {StarterCards, SubstituteCards} from '../'
+import { StarterCards, SubstituteCards } from '../'
+import './styles.css'
 
 const PlayersContainer = () => {
   return (
-    <main>
-      <section>
-      <h1>Starters</h1>
-      <StarterCards />
-      <h1>Substitutes</h1>
-      <SubstituteCards />
+    <main className='cards-container'>
+      <h2>Starters</h2>
+      <section className='starters-container'>
+        <StarterCards />
+      </section>
+      <h2>Substitutes</h2>
+      <section className='substitutes-container'>
+        <SubstituteCards />
       </section>
     </main>
   )
