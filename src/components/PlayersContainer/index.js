252bd275c37b24c145c654b60c4eb4678bc95e21
Krista Handel <k.handel66@gmail.com>
2018-08-17 19:00:29 -0600

    Restructure content

--- src/components/PlayersContainer/index.js
+++ src/components/PlayersContainer/index.js
@@ -3,12 +3,14 @@ import {StarterCards, SubstituteCards} from '../'
 
 const PlayersContainer = () => {
   return (
-    <section>
+    <main>
+      <section>
       <h1>Starters</h1>
       <StarterCards />
       <h1>Substitutes</h1>
       <SubstituteCards />
-    </section>
+      </section>
+    </main>
   )
 }
 
